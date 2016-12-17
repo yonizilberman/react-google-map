a5c6c096af88c0a6a477b4742f11a3f60d448667
Yoni <yoox21@gmail.com>
2016-12-17 13:51:11 +0200

    Create index.jsx

---
+++ app/src/index.jsx
@@ -0,0 +1,47 @@
+"use strict";
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+/*
+ * react object (class)
+ * */
+ var MapsHelper = require('./maps-helper');
+
+var AppClass = React.createClass({
+    mixins: [],
+    shouldComponentUpdate (nextProps, nextState) {
+        return true;
+    },
+    componentWillMount () {
+         (function (d, s, id) {
+             var js, fjs = d.getElementsByTagName(s)[d.getElementsByTagName(s).length - 1];
+             if (d.getElementById(id)) {
+             // if maps object as 
+                 return;
+             }
+             js = d.createElement(s);
+             js.id = id;
+             js.src = "https://maps.googleapis.com/maps/api/js?key=__KEY__&libraries=places&callback=initMap";
+             fjs.parentNode.insertBefore(js, fjs);
+         }(document, 'script', 'google-map-sdk'));
+
+         var initMap = window.initMap = () => {
+             this.showAppMap(google.maps);
+         };
+    },
+    showAppMap (googleMapsObject) {
+    	MapsHelper.init(googleMapsObject);
+    }
+    ,getInitialState () {
+        return {};
+    },
+    componentDidMount () {},
+    render () {
+        return (
+            <div id="googleMap" />
+        );
+    }
+});
+
+
+ReactDOM.render(<AppClass />, document.getElementById('body'));
