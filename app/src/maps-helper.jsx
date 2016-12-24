36b47dc36754418c4d84891c18fed9fa416ba576
Yoni <yoox21@gmail.com>
2016-12-24 23:35:30 +0200

    add marker on map
    now you can add marker on map and get location address (only one marker on the map)

--- app/src/maps-helper.jsx
+++ app/src/maps-helper.jsx
@@ -9,6 +9,8 @@
         geocoder: {},
         service: {},
 
+        marker: null,
+
 		init (googleMapsObject) {
 			__this.setMapObject(googleMapsObject);
 
@@ -20,6 +22,14 @@
 
             /* show user location if user allow share his location */
             userLocationOnMap();
+
+            // This event listener will call addMarker() when the map is clicked.
+            this.map.addListener('click', (event) => {
+                if (__this.marker != null) {
+                    __this.clearMarkerObject(__this.marker);
+                }
+                __this.addMarker(event.latLng, 'click');
+            });
 		},
         /*
          *
@@ -49,6 +59,86 @@
         /*   set google InfoWindow      */
         setInfoWindow: function () {
             __this.infoWindow = new __this.mapObject.InfoWindow();
+        },
+        /*   add marker to map          */
+        addMarker: function (location, mouseEvent) {
+            __this.geocoder.geocode({'location': location}, function (results, status) {
+                var locationText = 'Unavailable';
+                var flag = false;
+                if (status === __this.mapObject.GeocoderStatus.OK) {
+                    if (results[1]) {
+                        __this.service.getDetails({
+                            placeId: results[0].place_id
+                        }, function (place, status) {
+                            if (status === __this.mapObject.places.PlacesServiceStatus.OK) {
+                                locationText = place.formatted_address;
+                                flag = true;
+                            }
+                        });
+                    } else {
+                        console.log('No results found');
+                        console.log('location: ', location);
+                        flag = true;
+                    }
+                } else {
+                    console.log('Geocoder failed due to: ' + status);
+                    console.log('location: ', location);
+                    flag = true;
+                }
+                var locationIntervalTime = setInterval(function () {
+                    if (flag) {
+                        clearInterval(locationIntervalTime);
+                        flag = false;
+
+                        var options = {
+                            position: location,
+                            draggable: true
+                        };
+                        if (mouseEvent == 'click') {
+                            options.animation = __this.mapObject.Animation.DROP;
+                            __this.marker = _appMarker(options);
+                        } else if (mouseEvent == 'dragend') {
+                            __this.marker.setOptions(options);
+                        }
+
+                        __this.setMarkerOptions(
+                            _addLocation({address: ('<div data-lat="' + location.lat() + '" data-lng="' + location.lng() + '">' + locationText + '</div>')})
+                        );
+                    }
+                }, 300);
+            });
+        },
+        /*   set google Marker Options      */
+        setMarkerOptions: function (addLocation) {
+            __this.marker.setVisible(true);
+
+            __this.infoWindow.setContent(addLocation);
+            __this.infoWindow.open(__this.map, __this.marker);
+
+            // This event listener will call addMarker() when the map is drag end.
+            _addMapEventListener(__this.marker, 'dragend', function (event) {
+                __this.addMarker(event.latLng, 'dragend');
+            });
+
+            // then, remove the info windows name from the array
+            _addMapEventListener(__this.infoWindow, 'closeclick', function () {
+                __this.removerMarker(__this.marker, __this.infoWindow);
+            });
+        },
+        /*   clear marker's infoWindow from map      */
+        removerMarker: function (marker, infoWindow) {
+            __this.clearMarkerObject(marker);
+            window.setTimeout(function () {
+                __this.mapObject.event.clearListeners(infoWindow, 'closeclick'); // delay a bit to allow the other 'closeclick' event (that changes URL) to occur
+            }, 100);
+        },
+        /*   clear marker from map      */
+        clearMarkerObject: function (marker) {
+            if (marker) {
+                marker.setMap(null);
+                marker = null;
+            }
+            return;
         },
 	};
 
@@ -83,6 +173,33 @@
         infoWindow.setContent(browserHasGeolocation ?
             'Error: The Geolocation service failed.' :
             'Error: Your browser doesn\'t support geolocation.');
+    }
+
+    function _appMarker(markerOptions) {
+        markerOptions.map = __this.map;
+
+        return new __this.mapObject.Marker(markerOptions);
+    }
+
+    function _addLocation(location) {
+        /*
+         * address - $newLocation.find('.textArea div').text()
+         * label - $newLocation.find('input').val()
+         * thumbnail_file_id -
+         * latitude - $newLocation.find('.textArea > div').data('lat')
+         * longitude - $newLocation.find('.textArea > div').data('lng')
+         */
+        return (
+            '<div id="newLocation" class="marker-iw locationMarkerInfo">' +
+            '<div class="textArea">' +
+            location.address +
+            '</div>' +
+            '</div>'
+        );
+    }
+
+    function _addMapEventListener(element, event, callBack) {
+        return __this.mapObject.event.addListener(element, event, callBack);
     }
 
 	module.exports = __this = mapsHelper;
